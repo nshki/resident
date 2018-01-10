00bfc992b38a61a8e51a8a7c2f94de60e0284779
Nishiki Liu <nshki@users.noreply.github.com>
2018-01-10 01:57:58 +0000

    Don't use ENV.fetch in env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,9 +24,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    user_name: ENV.fetch('SENDGRID_USERNAME'),
-    password: ENV.fetch('SENDGRID_PASSWORD'),
-    domain: ENV.fetch('DOMAIN'),
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: ENV['DOMAIN'],
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
